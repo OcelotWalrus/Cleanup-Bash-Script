a31ce5c8f16b292841c7818c2340a9b9e34728b1
Cromha <87318892+OcelotWalrus@users.noreply.github.com>
2023-02-26 15:37:28 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -38,98 +38,98 @@ echo "Starting cleaning up system..."
 # Removing directories in settings.txt
 
 # Bash history
-if [ "$settings_line1" == "Bash_history=True" ]; then
+if [ "$settings_line1" == "Bash_history: True" ]; then
       history -c  >&- 2>&-
       echo "Deleted bash history..."
       sleep 1
 fi
 # Temporary files
 
-if [ "$settings_line2" == "Temporary_files=True" ]; then
+if [ "$settings_line2" == "Temporary_files: True" ]; then
       echo "Deleting temporary files..."
       rm -r /home/"$user"/tmp/  >&- 2>&-
       sleep 1
 fi
 
 # Discord cache
-if [ "$settings_line3" == "Discord_cache=True" ]; then
+if [ "$settings_line3" == "Discord_cache: True" ]; then
       echo "Deleting discord cache..."
       rm -r /home/"$user"/.config/discord/  >&- 2>&-
       sleep 1
 fi
 
 # Dnf autoremove
-if [ "$settings_line4" == "Dnf_autoremove=True" ]; then
+if [ "$settings_line4" == "Dnf_autoremove: True" ]; then
       echo "Running dnf autoremove..."
       sudo dnf autoremove -y  >&- 2>&-
       sleep 1
 fi
 
 # Dnf clean
-if [ "$settings_line5" == "Dnf_clean=True" ]; then
+if [ "$settings_line5" == "Dnf_clean: True" ]; then
       echo "Running dnf clean..."
       sudo dnf clean all -y  >&- 2>&-
       sleep 1
 fi
 
 # Firefox cache
-if [ "$settings_line6" == "Firefox_cache=True" ]; then
+if [ "$settings_line6" == "Firefox_cache: True" ]; then
       echo "Deleting firefox cache..."
       rm -r /home/"$user"/.cache/mozilla/firefox/  >&- 2>&-
       sleep 1
 fi
 
 # Firefox cookies
-if [ "$settings_line7" == "Firefox_cookies=True" ]; then
+if [ "$settings_line7" == "Firefox_cookies: True" ]; then
       echo "Deleting firefox cookies..."
       rm -r /home/"$user"/.mozilla/firefox/  >&- 2>&-
       sleep 1
 fi
 
 # Chrome cache
-if [ "$settings_line8" == "Chrome_cache=True" ]; then
+if [ "$settings_line8" == "Chrome_cache: True" ]; then
       echo "Deleting chrome cache..."
       rm -r "$user"/.cache/google-chrome/  >&- 2>&-
       sleep 1
 fi
 
 # Chrome crash reports
-if [ "$settings_line9" == "Chrome_cookies=True" ]; then
+if [ "$settings_line9" == "Chrome_cookies: True" ]; then
       echo "Deleting chrome crash reports..."
       rm -r /home/"$user"/.config/google-chrome/Crash \Reports/  >&- 2>&-
       sleep 1
 fi
 
 # Trash
-if [ "$settings_line10" == "Trash=True" ]; then
+if [ "$settings_line10" == "Trash: True" ]; then
       echo "Cleaning trash..."
       rm -rf /home/"$user"/.local/share/Trash/*  >&- 2>&-
       sleep 1
 fi
 
 # System cache
-if [ "$settings_line11" == "System_cache=True" ]; then
+if [ "$settings_line11" == "System_cache: True" ]; then
       echo "Deleting system cache..."
       rm -rf /home/"$user"/.cache/  >&- 2>&-
       sleep 1
 fi
 
 # System logs
-if [ "$settings_line12" == "System_logs=True" ]; then
+if [ "$settings_line12" == "System_logs: True" ]; then
       echo "Deleting system logs..."
       sudo rm -r /var/log/*
       sleep 1
 fi
 
 # Firefox history
-if [ "$settings_line13" == "Firefox_history=True" ]; then
+if [ "$settings_line13" == "Firefox_history: True" ]; then
       echo "Deleting firefox history..."
       rm -r /home/"$user"/.mozilla/firefox/  >&- 2>&-
       sleep 1
 fi
 
 # Chrome history
-if [ "$settings_line14" == "Chrome_history=True" ]; then
+if [ "$settings_line14" == "Chrome_history: True" ]; then
       echo "Deleting chrome history..."
       rm -r /home/"$user"/.config/google-chrome/Default/History/  >&- 2>&-
       rm -r /home/"$user"/.config/chromium/Default/History/ >&- 2>&-
